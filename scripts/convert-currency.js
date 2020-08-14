bc982ebb307ab9d6dcd7aa1daa17975e74520986
kevinnguyen <kevinnguyen@H1-Ryzentosh.local>
2020-08-14 13:46:16 -0700

    Add rates to settings

--- scripts/convert-currency.js
+++ scripts/convert-currency.js
@@ -13,13 +13,15 @@ Hooks.once("init", () => {
   });
   
 function patch_CurrencyConversion() {
+    rates = get_rates();
+
     Actor5e.prototype.convertCurrency = function () {
         const curr = duplicate(this.data.data.currency);
         const convert = {
-            cp: {into: "sp", each: 100},
-            sp: {into: "ep", each: 10 },
-            ep: {into: "gp", each: 10 },
-            gp: {into: "pp", each: 100}
+            cp: {into: "sp", each: rates["cp-sp"]},
+            sp: {into: "ep", each: rates["sp-ep"]},
+            ep: {into: "gp", each: rates["ep-gp"]},
+            gp: {into: "pp", each: rates["gp-pp"]}
         };
         for ( let [c, t] of Object.entries(convert) ) {
             let change = Math.floor(curr[c] / t.each);
@@ -30,12 +32,42 @@ function patch_CurrencyConversion() {
     };
 };
 
+function get_rates() {
+    return rates = {
+        cp_sp: game.settings.get("currencyConversion", "cp-sp"),
+        sp_ep: game.settings.get("currencyConversion", "sp-ep"),
+        ep_gp: game.settings.get("currencyConversion", "ep-gp"),
+        gp_pp: game.settings.get("currencyConversion", "gp-pp")
+    }
+}
+
 function conversion_rates() {
     game.settings.register("currencyConversion", "cp-sp", {
-        name: "Copper to Silver Rate",
+        name: "Copper to Silver",
         scope: "world",
         config: true,
-        default: false,
+        default: 10,
         type: Number
-      });
+    });
+    game.settings.register("currencyConversion", "sp-ep", {
+        name: "Silver to Electrum",
+        scope: "world",
+        config: true,
+        default: 5,
+        type: Number
+    });
+    game.settings.register("currencyConversion", "ep-gp", {
+        name: "Electrum to Gold",
+        scope: "world",
+        config: true,
+        default: 2,
+        type: Number
+    });
+    game.settings.register("currencyConversion", "gp-pp", {
+        name: "Gold to Platinum",
+        scope: "world",
+        config: true,
+        default: 10,
+        type: Number
+    });
 }
