412f318b52e6c7528c87c608c62bf2e7fd2b2a62
kevinnguyen <kevinnguyen@H1-Ryzentosh.local>
2020-08-26 19:26:54 -0700

    Reorganise and separate settings

---
+++ module/5e-exchange-rate.js
@@ -0,0 +1,36 @@
+// Import TypeScript modules
+import { registerSettings } from "./settings.js";
+
+Hooks.once("init", () => {
+    console.log("5e-exchange-rate | Init");
+
+    registerSettings();
+    console.log("5e-exchange-rate | Conversion Rates");
+});
+  
+Hooks.on("ready", function() {
+    console.log("5e-exchange-rate | Ready");
+    
+    patch_currencyConversion();
+    console.log("5e-exchange-rate | patch_currencyConversion")
+});
+  
+function patch_currencyConversion() {
+    let rates = get_conversion_rates();
+
+    CONFIG.DND5E.currencyConversion = {
+        cp: {into: "sp", each: rates["cp_sp"]},
+        sp: {into: "ep", each: rates["sp_ep"]},
+        ep: {into: "gp", each: rates["ep_gp"]},
+        gp: {into: "pp", each: rates["gp_pp"]}
+    }
+};
+
+function get_conversion_rates() {
+    return {
+        cp_sp: game.settings.get("5e-exchange-rate", "cp-sp"),
+        sp_ep: game.settings.get("5e-exchange-rate", "sp-ep"),
+        ep_gp: game.settings.get("5e-exchange-rate", "ep-gp"),
+        gp_pp: game.settings.get("5e-exchange-rate", "gp-pp")
+    }
+}
---
+++ module/settings.js
@@ -0,0 +1,31 @@
+export const registerSettings = function () {
+    // Register any custom module settings here
+    game.settings.register("5e-exchange-rate", "cp-sp", {
+        name: "Copper to Silver",
+        scope: "world",
+        config: true,
+        default: 10,
+        type: Number
+    });
+    game.settings.register("5e-exchange-rate", "sp-ep", {
+        name: "Silver to Electrum",
+        scope: "world",
+        config: true,
+        default: 5,
+        type: Number
+    });
+    game.settings.register("5e-exchange-rate", "ep-gp", {
+        name: "Electrum to Gold",
+        scope: "world",
+        config: true,
+        default: 2,
+        type: Number
+    });
+    game.settings.register("5e-exchange-rate", "gp-pp", {
+        name: "Gold to Platinum",
+        scope: "world",
+        config: true,
+        default: 10,
+        type: Number
+    });
+}
