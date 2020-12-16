01ff932d55019a8808e9d65ed86e6654a6666195
Kevin Nguyen <44279698+ktrieun@users.noreply.github.com>
2020-12-15 21:26:38 -0700

    Added Custom Currency Names

--- module/5e-exchange-rate.js
+++ module/5e-exchange-rate.js
@@ -12,7 +12,11 @@ Hooks.on("ready", function() {
     console.log("5e-exchange-rate | Ready");
     
     patch_currencyConversion();
-    console.log("5e-exchange-rate | patch_currencyConversion")
+    console.log("5e-exchange-rate | patch_currencyConversion");
+
+    patch_currencyNames();
+    console.log("5e-exchange-rate | patch_currencyNames");
+
 });
   
 function patch_currencyConversion() {
@@ -32,5 +36,27 @@ function get_conversion_rates() {
         sp_ep: game.settings.get("5e-exchange-rate", "sp-ep"),
         ep_gp: game.settings.get("5e-exchange-rate", "ep-gp"),
         gp_pp: game.settings.get("5e-exchange-rate", "gp-pp")
+    }
+}
+
+function patch_currencyNames() {
+    let altNames = fetchParams();
+
+    CONFIG.DND5E.currencies = {
+        "pp": altNames["ppAlt"],
+        "gp": altNames["gpAlt"],
+        "ep": altNames["epAlt"],
+        "sp": altNames["spAlt"],
+        "cp": altNames["cpAlt"]
+    };
+}
+
+function fetchParams() {
+    return {
+        cpAlt: game.settings.get("5e-exchange-rate", "cpAlt"),
+        spAlt: game.settings.get("5e-exchange-rate", "spAlt"),
+        epAlt: game.settings.get("5e-exchange-rate", "epAlt"),
+        gpAlt: game.settings.get("5e-exchange-rate", "gpAlt"),
+        ppAlt: game.settings.get("5e-exchange-rate", "ppAlt")
     }
 }
