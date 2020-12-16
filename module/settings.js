30d6cd5a6b7d1c1dfa553c4fe493fb7a56fb5078
Kevin Nguyen <44279698+ktrieun@users.noreply.github.com>
2020-12-15 21:27:20 -0700

    Added Custome Currency Name and Refresh on Change

--- module/settings.js
+++ module/settings.js
@@ -2,31 +2,74 @@ export const registerSettings = function () {
     // Register any custom module settings here
     game.settings.register("5e-exchange-rate", "cp-sp", {
         name: "Copper to Silver",
-        hint: "Refresh window to update converstion rates",
         scope: "world",
         config: true,
         default: 10,
-        type: Number
+        type: Number,
+        onChange: () => window.location.reload()
     });
     game.settings.register("5e-exchange-rate", "sp-ep", {
         name: "Silver to Electrum",
         scope: "world",
         config: true,
         default: 5,
-        type: Number
+        type: Number,
+        onChange: () => window.location.reload()
     });
     game.settings.register("5e-exchange-rate", "ep-gp", {
         name: "Electrum to Gold",
         scope: "world",
         config: true,
         default: 2,
-        type: Number
+        type: Number,
+        onChange: () => window.location.reload()
     });
     game.settings.register("5e-exchange-rate", "gp-pp", {
         name: "Gold to Platinum",
         scope: "world",
         config: true,
         default: 10,
-        type: Number
+        type: Number,
+        onChange: () => window.location.reload()
+    });
+    game.settings.register("5e-exchange-rate", "cpAlt", {
+        name: "Copper Alt Name",
+        scope: "world",
+        config: true,
+        default: "Copper",
+        type: String,
+        onChange: () => window.location.reload()
+    });
+    game.settings.register("5e-exchange-rate", "spAlt", {
+        name: "Silver Alt Name",
+        scope: "world",
+        config: true,
+        default: "Silver",
+        type: String,
+        onChange: () => window.location.reload()
+    });
+    game.settings.register("5e-exchange-rate", "epAlt", {
+        name: "Electrum Alt Name",
+        scope: "world",
+        config: true,
+        default: "Electrum",
+        type: String,
+        onChange: () => window.location.reload()
+    });
+    game.settings.register("5e-exchange-rate", "gpAlt", {
+        name: "Gold Alt Name",
+        scope: "world",
+        config: true,
+        default: "Gold",
+        type: String,
+        onChange: () => window.location.reload()
+    });
+    game.settings.register("5e-exchange-rate", "ppAlt", {
+        name: "Platinum Alt Name",
+        scope: "world",
+        config: true,
+        default: "Platinum",
+        type: String,
+        onChange: () => window.location.reload()
     });
 }
